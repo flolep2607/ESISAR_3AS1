c1fd3b19fd3d264e8316e601a5cae429b5f6086c
lol <lol@lol>
2022-10-13 14:48:51 +0200

    to be continued

--- CS351/src/TPChap5/exo2_poly.c
+++ CS351/src/TPChap5/exo2_poly.c
@@ -7,17 +7,41 @@ typedef struct
     int degres;
 } Terme;
 typedef Terme Polynome[NBMAX];
-void addpoly(Polynome p1,Polynome p2,Polynome res){
-    int index=0;
-    for(int i=0;i<NBMAX || p1[i].degres<0;i++){
-        for(int j=0;j<NBMAX;j++){
-            if(p1[i].degres == p2[i].degres){
+
+void addpoly(Polynome p1, Polynome p2, Polynome res) //virer boucle avec j ?
+{
+    int index = 0;
+    for (int i = 0; i < NBMAX || p1[i].degres < 0; i++)
+    {
+        for (int j = 0; j < NBMAX; j++)
+        {
+            if (p1[i].degres == p2[i].degres)
+            {
                 Terme tmp;
-                tmp.coef=p1[i].coef+p2[i].coef;
-                tmp.degres=p2[i].degres;
-                res[index]=tmp;
+                tmp.coef = p1[i].coef + p2[i].coef;
+                tmp.degres = p2[i].degres;
+                res[index] = tmp;
                 index++;
             }
         }
     }
+}
+
+int main()
+{
+    Terme a;
+    a.coef = 4;
+    a.degres = 3;
+    Terme b;
+    b.coef = 3;
+    b.degres = 2;
+    Terme c;
+    c.coef = 2;
+    c.degres = 1;
+    Terme d;
+    d.coef = 1;
+    d.degres = -1;
+    Polynome poly1 = {a, b, c, d};
+    Polynome res;
+    addpoly(poly1, poly1, res);
 }
