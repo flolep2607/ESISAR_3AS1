95674fd18ffbca9fc81b0ba0599d2011def636f3
lol <lol@lol>
2022-10-13 15:06:21 +0200

    ex2 chap 5 ok

--- CS351/src/TPChap5/exo2_poly.c
+++ CS351/src/TPChap5/exo2_poly.c
@@ -9,34 +9,37 @@ typedef struct
 typedef Terme Polynome[NBMAX];
 void addpoly(Polynome p1,Polynome p2,Polynome res){
     int index=0;
-    for(int i=0;i<NBMAX || p1[i].degres<0;i++){
-        for(int j=0;j<NBMAX || p2[j].degres<0;j++){
+    for(int i=0;i<NBMAX && p1[i].degres>0;i++){
+        for(int j=0;j<NBMAX && p2[j].degres>0;j++){
             if(p1[i].degres == p2[j].degres){
-                Terme tmp;
-                tmp.coef=p1[i].coef+p2[j].coef;
-                tmp.degres=p2[j].degres;
-                res[index]=tmp;
+                Terme * tmp = (Terme*) malloc(sizeof(Terme));
+                tmp->coef=p1[i].coef+p2[j].coef;
+                tmp->degres=p2[j].degres;
+                res[index]=*tmp;
                 index++;
             }
         }
     }
+    Terme * tmp = (Terme*) malloc(sizeof(Terme));
+    tmp->degres=-1;
+    res[index]=*tmp;
 }
 
 int main()
 {
-    Terme a;
-    a.coef = 4;
-    a.degres = 3;
-    Terme b;
-    b.coef = 3;
-    b.degres = 2;
+    Terme a = {4,3};
+    Terme b = {3,2};
     Terme c;
     c.coef = 2;
     c.degres = 1;
     Terme d;
     d.coef = 1;
     d.degres = -1;
-    Polynome poly1 = {a, b, c, d};
+    Polynome poly1 = {{4,3}, {3,2}, {1,1}, {1,-1}};
     Polynome res;
     addpoly(poly1, poly1, res);
+    for(int i=0;i<NBMAX && res[i].degres>0;i++){
+        printf("+(%fx^%d)",res[i].coef,res[i].degres);
+    }
+    printf("\n");
 }
