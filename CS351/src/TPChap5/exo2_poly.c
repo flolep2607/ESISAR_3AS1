8c56706d9e5752e8f2d85dea8d7946cb270d3097
flolep2607 <flo.lep2607@gmail.com>
2022-10-13 13:31:27 +0200

    Update exo2_poly.c

--- CS351/src/TPChap5/exo2_poly.c
+++ CS351/src/TPChap5/exo2_poly.c
@@ -10,14 +10,17 @@ typedef Terme Polynome[NBMAX];
 void addpoly(Polynome p1,Polynome p2,Polynome res){
     int index=0;
     for(int i=0;i<NBMAX || p1[i].degres<0;i++){
-        for(int j=0;j<NBMAX;j++){
-            if(p1[i].degres == p2[i].degres){
+        for(int j=0;j<NBMAX || p2[j].degres<0;j++){
+            if(p1[i].degres == p2[j].degres){
                 Terme tmp;
-                tmp.coef=p1[i].coef+p2[i].coef;
-                tmp.degres=p2[i].degres;
+                tmp.coef=p1[i].coef+p2[j].coef;
+                tmp.degres=p2[j].degres;
                 res[index]=tmp;
                 index++;
             }
         }
     }
+    Terme tmp;
+    tmp.degres=-1;
+    res[index]=tmp;
 }
