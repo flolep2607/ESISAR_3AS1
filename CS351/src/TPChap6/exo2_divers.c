25bff3c174223ba29e09ad2eda640148a7ee84f0
lol <lol@lol>
2022-10-14 14:09:21 +0200

    je dois finir les trucs sur les listes chainees (le cancer sans les pointeurs mdr)

--- CS351/src/TPChap6/exo2_divers.c
+++ CS351/src/TPChap6/exo2_divers.c
@@ -28,12 +28,35 @@ int pgcd_rec(int a, int b){
     }
 }
 
-/*char palindrome(char mot[], int taille){
-
-}*/
+char palindrome(char mot[], int taille, int compteur){ // compteur = 0
+    int myBool = 1;
+    if(mot[compteur] != mot[taille - 2 - compteur]){
+        myBool = -1;
+        printf("%s n'est pas un palindrome\n", mot);
+        return myBool;
+    }
+    else if(compteur == (taille-2)/2){
+        myBool = -1;
+        printf("%s n'est pas un palindrome\n", mot);
+        return myBool;
+    }
+    else{
+        palindrome(mot, taille, compteur+1);
+        //printf("%s est un palindrome\n", mot);
+    }
+    /*if (myBool == 1)
+    {
+        printf("%s est un palindrome\n", mot);
+    }
+    else{
+        printf("%s n'est pas un palindrome\n", mot);
+    }*/
+    return myBool;
+}
 
 int main(){
     //printf("%d\n", fibonacci(3));
-    printf("%d\n", pgcd_rec(16,4));
-    //palindrome("kayak", 5);
+    //printf("%d\n", pgcd_rec(16,4));
+    int size = sizeof("kayak")/sizeof(char);
+    palindrome("kayak", size, 0);
 }
