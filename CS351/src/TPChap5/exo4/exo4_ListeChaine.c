72f29e01894f83328d9caffc74fca9533a3f13a2
arcolinuxz <arcolinuxinfo@gmail.com>
2022-10-22 17:54:14 +0200

    ajout

---
+++ CS351/src/TPChap5/exo4/exo4_ListeChaine.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+typedef element *liste;
+
+int elementLibre(liste l)
+{
+}
+
+void creerListeVide(liste l)
+{
+	liste *L;
+	element *premier;
+	L[0] = premier;
+}
+
+void afficherListe(liste l)
+{
+	int size = sizeof(l) / sizeof(l[0]);
+	printf("(");
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d; ", l[i].valeur);
+	}
+	printf(")\n");
+}
+
+void insererElement(int x, liste l)
+{
+}
+
+void supprimerElement(int i, liste l)
+{
+}
+
+int main()
+{
+	element premier;
+	premier.valeur = 6;
+	element deuxieme;
+	deuxieme.valeur = 7;
+	premier.suivant = deuxieme.valeur;
+	deuxieme.valeur = 0;
+	liste *l_test;
+	l_test[0]->valeur = premier.valeur;
+	l_test[0]->suivant = premier.suivant;
+	l_test[1]->valeur = deuxieme.valeur;
+	l_test[1]->suivant = deuxieme.suivant;
+	afficherListe(*l_test);
+}
