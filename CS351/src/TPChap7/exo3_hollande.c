b00e51a5659a580d1b7b9c4a2d179549bb860ec3
lol <lol@lol>
2022-10-24 15:03:52 +0200

    Les pointeurs loul

--- CS351/src/TPChap7/exo3_hollande.c
+++ CS351/src/TPChap7/exo3_hollande.c
@@ -4,23 +4,23 @@
 // Lexique : b, w, r : entier ; T : tab[1..n] de couleur
 // Début
 // 	b ← 1; w ← 1 ; r ← n
-// 	tant que w ≤ r faire 
+// 	tant que w ≤ r faire
 // 		si T[w] = blanc alors w ← w+1
-// 		sinon si T[w] = bleu alors 
-// 			{échanger T[b] et T[w] ; 
+// 		sinon si T[w] = bleu alors
+// 			{échanger T[b] et T[w] ;
 // 			b ← b+1 ; w ← w+1 }
-// 		sinon // T[w] = rouge 
-// 			{échanger T[w] avec T[r] ; 
+// 		sinon // T[w] = rouge
+// 			{échanger T[w] avec T[r] ;
 // 			r ← r-1}
-// 	fintantque ; 
+// 	fintantque ;
 // Fin
 
-void tri(int tab[],int size){
+/*void tri(int tab[],int size){
     printf("size:%d\n",size);
     int b=0,w=0,r=size-1;
     int tmp;
     while(w<=r){
-        if(tab[w]>=3 && tab[w]<=6){ 
+        if(tab[w]>=3 && tab[w]<=6){
             w++;
         }else if(tab[w]<3){
             tmp=tab[b];
@@ -35,14 +35,44 @@ void tri(int tab[],int size){
             r--;
         }
     }
+}*/
+
+void tri(int *tab, int size)
+{
+    printf("size:%d\n", size);
+    int b = 0, w = 0, r = size - 1;
+    int tmp;
+    while (w <= r)
+    {
+        if (*(tab + w) >= 3 && *(tab + w) <= 6)
+        {
+            w++;
+        }
+        else if (*(tab + w) < 3)
+        {
+            tmp = *(tab + b);
+            *(tab + b) = *(tab + w);
+            *(tab + w) = tmp;
+            b++;
+            w++;
+        }
+        else
+        {
+            tmp = *(tab + r);
+            *(tab + r) = *(tab + w);
+            *(tab + w) = tmp;
+            r--;
+        }
+    }
 }
 
 int main()
 {
-    int tab[] = {5,8,1,4,3,9,2,7,3,8,1,4,5,3,8};
+    int tab[] = {5, 8, 1, 4, 3, 9, 2, 7, 3, 8, 1, 4, 5, 3, 8};
     int size = sizeof(tab) / sizeof(*tab);
-    tri(tab,size);
-    for (int i = 0; i < size; i++){
+    tri(tab, size);
+    for (int i = 0; i < size; i++)
+    {
         printf("%4d", tab[i]);
     }
     printf("\n");
