2867ce5331eeef760d4e04c5154bf8afb0a803c6
flolep2607 <a@a.a>
2022-10-25 08:40:16 +0200

    up

--- CS351/src/TPChap8/exo1/listechainee.h
+++ CS351/src/TPChap8/exo1/listechainee.h
@@ -1,1 +1,16 @@
-exo2/listechainee.h
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct elem
+{
+    int val;
+    struct elem* next;
+} element;
+
+void insertelement(int value,element* list);
+void delelement(int position,element* list);
+void affList(element* list);
+
+void freeAll(element* list);
+
+element* create_liste();
