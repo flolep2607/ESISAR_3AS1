f1e647ddbe1bed6c78e96ef2471c464219de41b9
flolep2607 <a@a.a>
2022-10-25 09:43:12 +0200

    up

--- CS351/src/TPChap8/exo2/main.c
+++ CS351/src/TPChap8/exo2/main.c
@@ -7,6 +7,11 @@ void insertator(element* liste){
     insertelement(5,1,1,liste);
     insertelement(-4,1,2,liste);
 }
+void insertator2(element* liste){
+    for(int x=0;x<liste->x;x++){
+        insertelement(1,x,x,liste);
+    }
+}
 element* somme_matrice(element* mat1,element* mat2){
     int max_x=mat1->x;
     int max_y=mat1->y;
@@ -25,13 +30,39 @@ element* somme_matrice(element* mat1,element* mat2){
     }
     return mat_somme;
 }
+element* prod_matrice(element* mat1,element* mat2){
+    if(mat1->y!=mat2->x){
+        printf("Les matrices sont pas multipliables\n");
+        exit(-1);
+    }
+    element* prod_somme=create_liste(mat1->x,mat2->y);
+    for(int y=0;y<mat1->y;y++){
+        for(int x=0;x<mat2->x;x++){
+            int value=0;
+            element* elem = mat1;
+            while(elem->next !=NULL)
+            {
+                elem = elem->next;
+                if(elem->y==y){
+                    value+=find_x_y(x,elem->x,mat2)+elem->val;
+                }
+            }
+            insertelement(value,x,y,prod_somme);
+        }
+    }
+    return prod_somme;
+}
 
 int main(){
     element* liste1=create_liste(4,4);
+    element* liste2=create_liste(4,4);
     insertator(liste1);
-    element* mat_sommed=somme_matrice(liste1,liste1);
+    insertator2(liste2);
+    affList(liste2);
+    element* mat_sommed=prod_matrice(liste1,liste2);
     affList(mat_sommed);
     freeAll(liste1);
+    freeAll(liste2);
     freeAll(mat_sommed);
     return 0;
 }
