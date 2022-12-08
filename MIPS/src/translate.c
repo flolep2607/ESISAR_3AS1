c54d3b7ef1f75abf0a29b443fdaf1a6b81cf7fa1
lol <lol@lol>
2022-12-08 10:51:01 +0100

    Koul ça marche

--- MIPS/src/translate.c
+++ MIPS/src/translate.c
@@ -14,6 +14,7 @@ uint32_t mask(uint8_t start, uint8_t end)
 uint32_t format_R_aled(uint8_t opcode, uint32_t args[], uint32_t special)
 {
   uint32_t result = 0;
+  result |= (special << 26) & mask(26, 30);
   result |= (args[0] << 11) & mask(11, 15);
   result |= opcode & mask(0, 10);
   return result;
@@ -29,10 +30,10 @@ uint32_t format_R_alternative(uint8_t opcode, uint32_t args[], uint32_t special)
 }
 uint32_t format_R_inversed(uint8_t opcode, uint32_t args[], uint32_t special)
 {
-  uint32_t tmp=args[0];
-  args[0]=args[1];
-  args[1]=args[0];
-  return format_R(opcode,args, special);
+  uint32_t tmp = args[0];
+  args[0] = args[1];
+  args[1] = tmp;
+  return format_R(opcode, args, special);
 }
 uint32_t format_R(uint8_t opcode, uint32_t args[], uint32_t special)
 {
@@ -70,46 +71,48 @@ uint32_t format_J(uint8_t opcode, uint32_t args[])
 }
 int find_instruction(char string[])
 {
-    uint8_t index_instruction = 0,i=0;
-    bool found = false;
-    while (!found && LISTE_INSTRUCT[index_instruction].name != NULL)
+  uint8_t index_instruction = 0, i = 0;
+  bool found = false;
+  while (!found && LISTE_INSTRUCT[index_instruction].name != NULL)
+  {
+    i = 0;
+    for (; LISTE_INSTRUCT[index_instruction].name[i] != 0 && string[i] != 0 && LISTE_INSTRUCT[index_instruction].name[i] == toupper(string[i]); i++)
     {
-        i = 0;
-        for (; LISTE_INSTRUCT[index_instruction].name[i] != 0 && string[i]!=0 && LISTE_INSTRUCT[index_instruction].name[i] == toupper(string[i]); i++)
-        {
-        }
-        if ((string[i] == ' ' || string[i] == 0) && LISTE_INSTRUCT[index_instruction].name[i] == 0)
-        {
-            found = true;
-            /*int j = i;
-            for (; string[j] != 0; j++)
-            {
-                string[j - i] = string[j];
-            }
-            string[j] = 0;*/
-        }
-        else
-        {
-            index_instruction++;
-        }
     }
-    if(!found){
-      printf("[ERROR] Instruction not found: %s",string);
-      exit(-1);
+    if ((string[i] == ' ' || string[i] == 0) && LISTE_INSTRUCT[index_instruction].name[i] == 0)
+    {
+      found = true;
+      /*int j = i;
+      for (; string[j] != 0; j++)
+      {
+          string[j - i] = string[j];
+      }
+      string[j] = 0;*/
     }
-    return index_instruction;
+    else
+    {
+      index_instruction++;
+    }
+  }
+  if (!found)
+  {
+    printf("[ERROR] Instruction not found: %s", string);
+    exit(-1);
+  }
+  return index_instruction;
 }
 
-uint32_t* parse_parameters(char *string, int index, int nb_arg_max){
+uint32_t *parse_parameters(char *string, int index, int nb_arg_max)
+{
   bool address = false;
-  uint32_t* args = malloc(sizeof(uint32_t)*nb_arg_max);
+  uint32_t *args = malloc(sizeof(uint32_t) * nb_arg_max);
   int length;
   int nb_arg = 0;
   while (string[index] != '\0' && nb_arg < nb_arg_max)
   {
     if (string[index] == ' ')
     {
-      index ++;
+      index++;
     }
     else if (string[index] == '$')
     {
@@ -120,80 +123,84 @@ uint32_t* parse_parameters(char *string, int index, int nb_arg_max){
     {
       index++;
     }
-    else {
+    else
+    {
       if (address)
       {
-        sscanf(string+index,"%u%n", &args[nb_arg], &length);
+        sscanf(string + index, "%u%n", &args[nb_arg], &length);
       }
-      else {
-        sscanf(string+index,"%u%n", &args[nb_arg], &length);
+      else
+      {
+        sscanf(string + index, "%u%n", &args[nb_arg], &length);
       }
-      if (length == 0){
+      if (length == 0)
+      {
         if (address)
         {
           args[nb_arg] = 0;
         }
-        //TODO
-        // else {
-        //   char *stringmachin;
-        //   sscanf("%s",string[index], stringmachin);
-        //   args[nb_arg] = label_to_adress(stringmachin);
-        // }
+        // TODO
+        //  else {
+        //    char *stringmachin;
+        //    sscanf("%s",string[index], stringmachin);
+        //    args[nb_arg] = label_to_adress(stringmachin);
+        //  }
       }
       nb_arg++;
       index += length;
-      address=false;
+      address = false;
     }
   }
   return args;
 }
 
-uint32_t translate_line(char string[]){
-    uint8_t index_instruction=find_instruction(string);
-    uint32_t resultat = 0;
-    // uint32_t arg1, arg2, arg3;
-    //! faut refaire ce truc
-    uint32_t* parameters;
-    //TODO just 1 R malloc here
-    //TODO 
-    switch (LISTE_INSTRUCT[index_instruction].format)
-    {
-    case J:
-        parameters=parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 2);
-        resultat = format_J(LISTE_INSTRUCT[index_instruction].opcode, parameters);
-        break;
-    case R_reverse:
-        parameters=parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 3);
-        resultat = format_R_inversed(LISTE_INSTRUCT[index_instruction].opcode, parameters, LISTE_INSTRUCT[index_instruction].special);
-        break;
-    case R_alternative:
-        parameters=parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 2);
-        resultat = format_R_alternative(LISTE_INSTRUCT[index_instruction].opcode, parameters, LISTE_INSTRUCT[index_instruction].special);
-        break;
-    case R_aled:
-        parameters=parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 1);
-        resultat = format_R_aled(LISTE_INSTRUCT[index_instruction].opcode, parameters, LISTE_INSTRUCT[index_instruction].special);
-        break;
-    case R:
-        parameters=parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 3);
-        resultat = format_R(LISTE_INSTRUCT[index_instruction].opcode, parameters, LISTE_INSTRUCT[index_instruction].special);
-        break;
-    case I:
-        parameters=parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 3);
-        resultat = format_I(LISTE_INSTRUCT[index_instruction].opcode, parameters);
-        break;
-    case I_no_first:
-        parameters=parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 2);
-        resultat = format_I_no_first(LISTE_INSTRUCT[index_instruction].opcode, parameters);
-        break;
+uint32_t translate_line(char string[])
+{
+  uint8_t index_instruction = find_instruction(string);
+  uint32_t resultat = 0;
+  // uint32_t arg1, arg2, arg3;
+  //! faut refaire ce truc
+  uint32_t *parameters;
+  // TODO just 1 R malloc here
+  // TODO
+  switch (LISTE_INSTRUCT[index_instruction].format)
+  {
+  case J:
+    parameters = parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 2);
+    resultat = format_J(LISTE_INSTRUCT[index_instruction].opcode, parameters);
+    break;
+  case R_reverse:
+    parameters = parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 3);
+    resultat = format_R_inversed(LISTE_INSTRUCT[index_instruction].opcode, parameters, LISTE_INSTRUCT[index_instruction].special);
+    break;
+  case R_alternative:
+    parameters = parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 2);
+    resultat = format_R_alternative(LISTE_INSTRUCT[index_instruction].opcode, parameters, LISTE_INSTRUCT[index_instruction].special);
+    break;
+  case R_aled:
+    parameters = parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 1);
+    resultat = format_R_aled(LISTE_INSTRUCT[index_instruction].opcode, parameters, LISTE_INSTRUCT[index_instruction].special);
+    break;
+  case R:
+    parameters = parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 3);
+    resultat = format_R(LISTE_INSTRUCT[index_instruction].opcode, parameters, LISTE_INSTRUCT[index_instruction].special);
+    break;
+  case I:
+    parameters = parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 3);
+    resultat = format_I(LISTE_INSTRUCT[index_instruction].opcode, parameters);
+    break;
+  case I_no_first:
+    parameters = parse_parameters(string, strlen(LISTE_INSTRUCT[index_instruction].name), 2);
+    resultat = format_I_no_first(LISTE_INSTRUCT[index_instruction].opcode, parameters);
+    break;
 
-    default:
-        printf("IDK this command\n");
-        exit(-1);
-        break;
-    }
-    free(parameters);
-    return resultat;
+  default:
+    printf("IDK this command\n");
+    exit(-1);
+    break;
+  }
+  free(parameters);
+  return resultat;
 }
 // string : (" $4,$3,2",1)=> "3"
 /*uint8_t get_args(char *string, int arg_number)
