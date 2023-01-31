63934157504dfc10ce7c757b3c08d0a0eb98186b
Flo <cenestpasmonmail@hidden.truc>
2023-01-31 08:31:05 +0100

    rework

--- CS351/src/TPChap4/exo1/main.c
+++
@@ -1,9 +0,0 @@
-#include "module.h"
-int main(int argc, char * argv[])
-{
-    int T[10] = {0};
-    int nb = lireDonnes(argv[1], T);
-    trieABulles(T, nb);
-    afficherTableau(T, nb);
-    enregistrerdonnees(argv[2],T,nb);
-}
