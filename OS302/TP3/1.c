e427c555a5a422c4cc70d6c8e0df4cf124201920
Flo <cenestpasmonmail@hidden.truc>
2023-03-28 09:42:52 +0200

    OS/TP3/1.c

--- OS302/TP3/1.c
+++ OS302/TP3/1.c
@@ -1,35 +1,37 @@
+#include <sys/types.h>
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <signal.h>
-void o(){}
-int main(int argc, char *argv[])
-{
-    if (argc != 3) {
-		printf("Utilisation : %s executable !\n", argv[0]);
-		return EXIT_FAILURE;
+#include <sys/wait.h>
+#include <assert.h>
+
+int main (int argc, char *argv[]) {
+	
+	pid_t pid;
+	int fd[2];	
+	pipe(fd);
+	
+	assert(argc==3);
+	if ((pid=fork())>0) { // pere
+		waitpid(pid,NULL,0);
+		
+		close(fd[1]);
+		dup2 (fd[0], STDIN_FILENO);
+		system(argv[2]);
+        close(fd[0]);
 	}
-    int pipefd[2];
-    pipe(pipefd);
-    pid_t ret = fork();
-    if (ret == 0)
-    {
-        close(pipefd[0]);
-        dup2(pipefd[1],STDOUT_FILENO);
-        system(argv[1]);
-        close(pipefd[1]);
-    }
-    else
-    {   
-        waitpid(ret, NULL, 0);
-        close(pipefd[1]);
-        dup2(pipefd[0],STDIN_FILENO);
-        printf("%s\n", argv[2]);
-        system(argv[2]);
-        //TODO: faire marcher cette merde
-    }
+	else if (pid == 0)// fils 
+	{
+		close(fd[0]);
+		dup2 (fd[1], STDOUT_FILENO);
+		system(argv[1]);
+        close(fd[1]);
+	}
+	else {
+		perror("Creation de processus");
+		exit(2);
+	}
+
+
+	exit(0);
 }
