04363b530153121725bc9055ca0f0f4a51c3e8e8
lol <lol@lol>
2023-03-29 16:16:43 +0200

    o

--- CS353/TP3/test.c
+++ CS353/TP3/test.c
@@ -3,59 +3,92 @@
  ******************************************************************************/
 
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "store.h"
 
 /*----------------------------------------------------------------------------
  * Cette fonction Compte le nombre d'essai avant d'arriver sur l'item.
  *----------------------------------------------------------------------------*/
 
-int testInsert() {
-    int r, ind = 0;
-    int tab[TABLE_SIZE];
-    Result *res;
+// Liste chaînée faite pour le type Result (il s'agit d'une liste chaînée d'Items)
 
-    r = 0;
-    ind = 0;
-    // if ( insertItem(300,"ToFind",r*1.0) == SUCCESS ) { tab[r]=ind; r++; }
-    // if ( insertItem(400,"ToFind",r*1.0) == SUCCESS ) { tab[r]=ind; r++; }
+typedef struct structResult
+{
+    Item *item;
+    struct structResult *next;
+} Result;
 
-    while (r < TABLE_SIZE) {
-        ind = rand() % 50000;
-        if (insertItem(ind, "NONAME", r * 1.0) == SUCCESS) {
-            tab[r] = ind;
-            r++;
-        }
-    }
+// int testInsert()
+// {
+//     int r, ind = 0;
+//     int tab[TABLE_SIZE];
+//     Result *res;
 
-    printf("Insert OK\n");
-    for (r = 0; r < TABLE_SIZE / 100; r++) {
-        if (updateItem(tab[r], "ToFind", r * 1.0) != SUCCESS) return 0;
-    }
+//     r = 0;
+//     ind = 0;
+//     // if ( insertItem(300,"ToFind",r*1.0) == SUCCESS ) { tab[r]=ind; r++; }
+//     // if ( insertItem(400,"ToFind",r*1.0) == SUCCESS ) { tab[r]=ind; r++; }
 
-    printf("Update OK\n");
-    res = findItem("ToFind");
-    while (res) {
-        printf("res=%d prix %.2f\n", res->item->code, res->item->price);
-        res = res->next;
-    }
-    res = NULL;
+//     while (r < TABLE_SIZE)
+//     {
+//         ind = rand() % 50000;
+//         if (insertItem(ind, "NONAME", r * 1.0) == SUCCESS)
+//         {
+//             tab[r] = ind;
+//             r++;
+//         }
+//     }
 
-    res = findItemWithIndex("ToFind");
-    while (res) {
-        printf("Index res=%d prix %.2f\n", res->item->code, res->item->price);
-        res = res->next;
-    }
+//     printf("Insert OK\n");
+//     // dumpItems();
+//     for (r = 0; r < TABLE_SIZE / 100; r++)
+//     {
+//         if (updateItem(tab[r], "ToFind", r * 1.0) != SUCCESS)
+//             return 0;
+//     }
+
+//     printf("Update OK\n");
+//     dumpItems();
+//     // res = *getItem("ToFind");
+//     // while (res)
+//     // {
+//     //     printf("res=%d prix %.2f\n", res->item->code, res->item->price);
+//     //     res = res->next;
+//     // }
+//     // res = NULL;
+
+//     // res = *getItemWithIndex("ToFind");
+//     // while (res)
+//     // {
+//     //     printf("Index res=%d prix %.2f\n", res->item->code, res->item->price);
+//     //     res = res->next;
+//     // }
+//     return 1;
+// }
+
+int testInsertButActuallyGood()
+{
+    insertItem(1000, "Sucre", 20.50);
+    insertItem(60, "Sel", 695.23);
+    insertItem(995, "Poudre bleue", 999.99);
+    //dumpItems();
+    suppressItem(995);
+    insertItem(666, "Fromage", 59.23);
+    //dumpItems();
+    updateItem(60, "Sel de Guérin", 69.52);
+    dumpItems();
+    return 0;
 }
 
-int main() {
-    int nb = 0, r;
+int main()
+{
+    // int nb = 0, r;
     //
     printf("****** Gestionnaire de magasin ******\n");
     //
     init();
     //
-    testInsert();
+    testInsertButActuallyGood();
     printf("=======Gestionnaire de magasin ======\n");
 
     return 0;
